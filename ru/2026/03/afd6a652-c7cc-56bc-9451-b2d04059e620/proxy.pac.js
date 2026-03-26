7228e052a3605572c1ecc735a96c58fdaf7b4d10
ghpbot <ghpbot@bk.ru>
2026-03-26 08:18:15 +0000

    deploy: s1775/site@257589c0ff96c1e7db13517b71fa1a0d5fe18dbe

---
+++ ru/2026/03/afd6a652-c7cc-56bc-9451-b2d04059e620/proxy.pac.js
@@ -0,0 +1,63 @@
+const proxyServer = 'HTTP proxy.example.org:3128; SOCKS5 proxy.example.org:1080; PROXY proxy.example.org:8080; DIRECT';
+const dnsCache = {};
+
+const localTldStorage = [
+  '.home.arpa',
+  '.internal',
+];
+
+const directTldStorage = [
+  '.am',
+  '.az',
+  '.by',
+  '.kg',
+  '.kz',
+  '.md',
+  '.mn',
+  '.ru',
+  '.su',
+  '.tatar',
+  '.tj',
+  '.tm',
+  '.uz',
+  '.xn--p1ai', // .рф
+];
+
+function fastResolve(host) {
+  let ip = dnsResolve(host);
+  if (dnsCache[host]) return dnsCache[host];
+  if (ip) dnsCache[host] = ip;
+  return ip;
+}
+
+function FindProxyForURL(url, host) {
+  url = url.toLowerCase();
+  host = host.toLowerCase();
+
+  if (isPlainHostName(host)) return 'DIRECT';
+
+  let ip = fastResolve(host);
+
+  if (
+    isInNet(ip, '127.0.0.0', '255.0.0.0') ||
+    isInNet(ip, '10.0.0.0', '255.0.0.0') ||
+    isInNet(ip, '172.16.0.0', '255.240.0.0') ||
+    isInNet(ip, '192.168.0.0', '255.255.0.0')
+  ) {
+    return 'DIRECT';
+  }
+
+  if (localTldStorage.length !== 0) {
+    for (let i = 0; i < localTldStorage.length; i++) {
+      if (dnsDomainIs(host, localTldStorage[i])) return 'DIRECT';
+    }
+  }
+
+  if (directTldStorage.length !== 0) {
+    for (let i = 0; i < directTldStorage.length; i++) {
+      if (dnsDomainIs(host, directTldStorage[i])) return 'DIRECT';
+    }
+  }
+
+  return proxyServer;
+}
